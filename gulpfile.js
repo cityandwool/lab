6466b4538270aad0122ded50243c0731c5588abe
Dan Ryland <studio@cityandwool.com>
2014-04-04 02:04:22 +0100

    Gulp, gitignore and styling

---
+++ gulpfile.js
@@ -0,0 +1,21 @@
+var gulp = require('gulp'),
+	compass = require('gulp-compass'),
+	watch = require('gulp-watch'),
+    path = require('path');
+
+gulp.task('compass', function() {
+    gulp.src('./assets/sass/*.scss')
+        .pipe(compass({
+            project: path.join(__dirname, 'assets'),
+            css: 'css',
+            sass: 'sass'
+        }))
+        .pipe(gulp.dest('assets/css'));
+});
+
+gulp.task('watch', function() {
+    gulp.watch('assets/sass/*.scss', ['compass']);
+});
+
+// Default Task
+gulp.task('default', ['watch']);
