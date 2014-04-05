2382e42b55259e51e97b1d2411ae9f32341dc808
Dan Ryland <studio@cityandwool.com>
2014-04-05 14:41:39 +0100

    First list item styled

--- assets/js/living-list.js
+++ assets/js/living-list.js
@@ -8,13 +8,36 @@ $(document).ready(function() {
 		$(this).parent().addClass('visited');
 	}
 
+	function hasLivingList() {
+		var $ribbon = $('.post-1 a');
+		var star = '<div class="star"></div>';
+		$ribbon.append(star);
+	}
+	function hasClearToDo() {
+		$('.star').remove();
+	}
+
 	function styleSwitch() {
 		$body.toggleClass('living-list clear-to-do');
 		$js_switch.parent().toggleClass('active');
+		if($body.hasClass('living-list')) {
+			hasLivingList();
+		}
+		if($body.hasClass('clear-to-do')) {
+			hasClearToDo();
+		}
 		return false;
+	}
+
+	if($body.hasClass('living-list')) {
+		hasLivingList();
+	}
+	if($body.hasClass('clear-to-do')) {
+		hasClearToDo();
 	}
 
 	$postLink.on('click',visited);
 	$js_switch.on('click',styleSwitch);
+	
 
 });
